e002349111d9ebd4736b97438166040ae6a94f63
Kolby Kucera <kolbykuc@iastate.edu>
2023-02-01 19:22:19 -0600

    This is my progress on the experiment I have been working on the past few days. It uses the CRUDL method. Basic user creation. More to come later.

---
+++ Experiments/KolbyExperiments/Experiment2/src/main/java/com/example/experiment2/User.java
@@ -0,0 +1,41 @@
+package com.example.experiment2;
+
+public class User {
+
+    private String username;
+    private String password;
+    private String accountType;
+
+
+    public User(String username, String password, String accountType, String displayName){
+
+        this.username = username;
+        this.password = password;
+        this.accountType = accountType;
+
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getAccountType(){
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
