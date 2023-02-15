6e22a2283ecd97ebd78222aa7bf835478a308c54
cferris <connor27@iastate.edu>
2023-02-15 01:13:19 -0600

    JSON Postman test

--- Experiments/ConnorExperiments/AndroidVolley/app/src/main/java/com/example/sumon/androidvolley/utils/Const.java
+++ Experiments/ConnorExperiments/AndroidVolley/app/src/main/java/com/example/sumon/androidvolley/utils/Const.java
@@ -1,8 +1,10 @@
 package com.example.sumon.androidvolley.utils;
 
 public class Const {
-	public static final String URL_JSON_OBJECT = "https://jsonplaceholder.typicode.com/users/1";
+	public static final String URL_JSON_OBJECT = "https://c345266a-448f-4b47-a9ae-9167630a6509.mock.pstmn.io/fruits/";
+	//"https://jsonplaceholder.typicode.com/users/1"
 	public static final String URL_JSON_ARRAY = "https://jsonplaceholder.typicode.com/users";
 	public static final String URL_STRING_REQ = "https://jsonplaceholder.typicode.com/users/1";
-	public static final String URL_IMAGE = "http://sharding.org/outgoing/temp/testimg3.jpg";
+	public static final String URL_IMAGE = "https://media.licdn.com/dms/image/D5635AQF39x8yqJP-eQ/profile-framedphoto-shrink_400_400/0/1676147347196?e=1677049200&v=beta&t=0U3j2GaUW6NplMmAz8OUB9gt_j8oY6krOvOwlO7kEkE";
+	//http://sharding.org/outgoing/temp/testimg3.jpg
 }
