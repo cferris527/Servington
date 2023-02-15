88ebc07c3a3cb0f82280cc32d36960e1ec781ed9
Kolby Kucera <kolbykuc@iastate.edu>
2023-02-15 15:40:02 -0600

    Backend skeleton

---
+++ Backend/InitialBackend/src/main/java/com/example/experiment1/Experiment1Application.java
@@ -0,0 +1,34 @@
+package com.example.experiment1;
+
+import com.example.experiment1.User.User;
+import com.example.experiment1.User.UserController;
+import com.example.experiment1.User.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories
+@SpringBootApplication
+public class Experiment1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Experiment1Application.class, args);
+
+    }
+
+    @Bean
+    CommandLineRunner initUser(UserRepository userRepository) {
+        return args -> {
+            User user1 = new User("kolbykuc", "password", "volunteer" );
+            User user2 = new User("Jane", "123", "ORG");
+            User user3 = new User("Justin", "hello", "volunteer");
+            userRepository.save(user1);
+            userRepository.save(user2);
+            userRepository.save(user3);
+
+        };
+    }
+
+}
---
+++ Backend/InitialBackend/src/main/java/com/example/experiment1/User/User.java
@@ -0,0 +1,62 @@
+package com.example.experiment1.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String accountType;
+
+    private String username;
+
+    private String password;
+
+    public User(){
+
+    }
+
+    public User(String username, String password, String accountType){
+        this.username = username;
+        this.password = password;
+        this.accountType = accountType;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUsername(String name) {
+        this.username = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+}
