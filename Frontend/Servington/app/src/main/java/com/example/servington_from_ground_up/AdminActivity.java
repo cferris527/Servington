0d5e95caaf2dc9e062e0e6faf08c2c2ef8716007
cferris <connor27@iastate.edu>
2023-02-28 14:48:51 -0600

    -

--- Frontend/Servington/app/src/main/java/com/example/servington_from_ground_up/AdminActivity.java
+++ Frontend/Servington/app/src/main/java/com/example/servington_from_ground_up/AdminActivity.java
@@ -1,14 +1,39 @@
 package com.example.servington_from_ground_up;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AdminActivity extends AppCompatActivity {
 
+    private Button manageButton;
+    private Button deleteButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        manageButton = findViewById(R.id.manageButton);
+        deleteButton = findViewById(R.id.deleteButton);
+
+        manageButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                //TODO: admin account creation activity
+            }
+        });
+
+        deleteButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                //TODO: post/account deletion activity
+            }
+        });
     }
 }
---
+++ Frontend/Servington/app/src/main/java/com/example/servington_from_ground_up/ManageAdminsActivity.java
@@ -0,0 +1,14 @@
+package com.example.servington_from_ground_up;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ManageAdminsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_admins);
+    }
+}
