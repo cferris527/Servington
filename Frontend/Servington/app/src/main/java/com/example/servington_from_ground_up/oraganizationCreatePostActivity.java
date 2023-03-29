661937f4db93a00031b37a87faef6c2869d7642b
mike wailes <mlwailes@iastate.edu>
2023-03-29 14:59:36 -0500

    initial commit for create post page

---
+++ Frontend/Servington/app/src/main/java/com/example/servington_from_ground_up/oraganizationCreatePostActivity.java
@@ -0,0 +1,15 @@
+package com.example.servington_from_ground_up;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class oraganizationCreatePostActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceStates)
+    {
+        super.onCreate(savedInstanceStates);
+        setContentView(R.layout.activity_create_post);
+
+    }
+}
