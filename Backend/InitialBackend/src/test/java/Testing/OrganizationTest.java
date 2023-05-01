9d7576c6b2ec198b92078463cd93230e20149560
Kolby Kucera <kolbykuc@iastate.edu>
2023-04-30 23:41:37 -0500

    current tests

--- Backend/InitialBackend/src/test/java/Testing/OrganizationTest.java
+++ Backend/InitialBackend/src/test/java/Testing/OrganizationTest.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Scanner;
 
+import com.example.experiment1.Organization.Organization;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -22,6 +23,7 @@ import org.json.JSONObject;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
@@ -94,6 +96,64 @@ public class OrganizationTest {
                 log().all().
                 assertThat().
                 statusCode(200);
+    }
+
+    @Test
+    public void test_editFields() throws JSONException {
+        JSONObject admin  = new JSONObject();
+        admin.put("id", 2);
+        admin.put("password", "pass");
+        admin.put("displayName", "test");
+        admin.put("email", "test");
+        admin.put("phone_number", "test");
+
+
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(admin.toString())
+                .log().all()
+
+                .when()
+                .post("http://localhost:8080/orgEditFields")
+
+                .then()
+                .assertThat().statusCode(200)
+                .log().all();
+
+    }
+
+    @Test
+    public void test_banOrg() throws Throwable {
+        given().
+                when().
+                post("/banOrganization/Orgy").
+                then().
+                log().all().
+                assertThat().
+                statusCode(200);
+    }
+
+    @Test
+    public void test_unbanOrg() throws Throwable {
+        given().
+                when().
+                post("/unbanOrganization/Orgy").
+                then().
+                log().all().
+                assertThat().
+                statusCode(200);
+    }
+
+    @Test
+    public void test_Org() throws Throwable {
+        Organization o = new Organization("please", "please", "please", "please", "please","please");
+
+        Assertions.assertEquals(o.getEmail(), "please");
+        Assertions.assertEquals(o.getPassword(), "please");
+        Assertions.assertEquals(o.getUsername(), "please");
+        Assertions.assertEquals(o.getDisplayName(), "please");
+        Assertions.assertEquals(o.getPhone_number(), "please");
     }
 
 
--- Backend/InitialBackend/src/test/java/Testing/VolunteerTest.java
+++ Backend/InitialBackend/src/test/java/Testing/VolunteerTest.java
@@ -9,6 +9,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Scanner;
 
+import com.example.experiment1.Organization.Organization;
+import com.example.experiment1.Volunteer.Volunteer;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -22,6 +24,7 @@ import org.json.JSONObject;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
@@ -94,6 +97,64 @@ public class VolunteerTest {
                 log().all().
                 assertThat().
                 statusCode(200);
+    }
+
+    @Test
+    public void test_editFields() throws JSONException {
+        JSONObject admin  = new JSONObject();
+        admin.put("id", 1);
+        admin.put("password", "pass");
+        admin.put("displayName", "test");
+        admin.put("email", "test");
+        admin.put("phone_number", "test");
+
+
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(admin.toString())
+                .log().all()
+
+                .when()
+                .post("http://localhost:8080/volunteerEditFields")
+
+                .then()
+                .assertThat().statusCode(200)
+                .log().all();
+
+    }
+
+    @Test
+    public void test_banVol() throws Throwable {
+        given().
+                when().
+                post("/banVolunteer/John").
+                then().
+                log().all().
+                assertThat().
+                statusCode(200);
+    }
+
+    @Test
+    public void test_unbanVol() throws Throwable {
+        given().
+                when().
+                post("/unbanVolunteer/John").
+                then().
+                log().all().
+                assertThat().
+                statusCode(200);
+    }
+
+    @Test
+    public void test_Org() throws Throwable {
+        Volunteer v = new Volunteer("please", "please", "please", "please", "please","please");
+
+        Assertions.assertEquals(v.getEmail(), "please");
+        Assertions.assertEquals(v.getPassword(), "please");
+        Assertions.assertEquals(v.getUsername(), "please");
+        Assertions.assertEquals(v.getDisplayName(), "please");
+        Assertions.assertEquals(v.getPhone_number(), "please");
     }
 
 
